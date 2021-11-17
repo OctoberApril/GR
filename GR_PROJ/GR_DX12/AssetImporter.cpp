ec8a65029b545994fb69ec3c0da6f96b8c244da3
honghaiqi <954034836@qq.com>
2021-11-17 16:16:11 +0800

    refactor solution

--- GR_PROJ/GR_DX12/AssetImporter.cpp
+++ GR_PROJ/GR_DX12/AssetImporter.cpp
@@ -2,9 +2,9 @@
 
 #include <iostream>
 #include "DefaultMaterial.h"
-#include "assimp/Importer.hpp"
-#include "assimp/postprocess.h"
-#include "assimp/scene.h"
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
 #include "GameObject.h"
 #include "Material.h"
 #include "Mesh.h"
